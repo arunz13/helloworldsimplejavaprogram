5179f2341aeebcd6dad515bccfa2fcfbf2e9299b
arunz13 <36954691+arunz13@users.noreply.github.com>
2024-02-05 07:10:31 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class Simple{
+    public static void main(String args[]){
+        System.out.println("Hello students");
+      System.out.println("Welcome to continous integration with Jenkins");
+    } 
+}
